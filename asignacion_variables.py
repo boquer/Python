fbf14b0bff93bec632aaae2ddc8c97195b565573
boquer <dani_981103@hotmail.com>
2019-12-27 19:10:42 -0600

    1- Variables

---
+++ asignacion_variables.py
@@ -0,0 +1,26 @@
+"""Se almacenan los datos en las respectivas variables"""
+entero = 6
+flotante = 4.5
+cadena = "hola mundo"
+caracter = 'c'
+
+print(entero) #me imprime el valor de la variable.
+print(type(entero)) #me dice de que tipo es mi variable
+
+print(flotante)
+print(type(flotante))
+
+print(cadena)
+print(type(cadena))
+
+print(caracter)
+print(type(caracter))
+
+
+"""Tipado dinámico"""
+variable = 10 #primero se le asigna un número entero y lo imprime.
+print(variable)
+
+
+variable="Daniela" #a la misma variable se le asigna una cadena y la imprime.
+print(variable)
