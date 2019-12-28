f4db9075f6d9e67351d09109380135be99da36af
boquer <dani_981103@hotmail.com>
2019-12-27 19:11:34 -0600

    2- Entrada de Datos

---
+++ entrada_datos.py
@@ -0,0 +1,12 @@
+nombre = input("Ingresa tu nombre: ") #Le pide al usuario que ingrese su nombre y lo va  aguardar en la variable nombre.
+
+edad = int(input("Ingersa tu edad: ")) #le pide al usuario que ingrese su edad y lo guarda en la variable edad.
+
+decimal = float(input("Ingresa un numero decimal: "))
+
+
+"""Impresion de los datos"""
+
+print("\nTu nombre es: ", nombre)
+print("\nTienes ", edad, "años")
+print("\nEl número decimal que ingresaste es: ", decimal)
