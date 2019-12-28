f7fbfd38ef1ef28fc9c1e119f38bd7e48560bb51
boquer <dani_981103@hotmail.com>
2019-12-27 19:14:38 -0600

    5- Listas

---
+++ listas.py
@@ -0,0 +1,16 @@
+#Las listas en python son como los arreglos que se encuentran en los diferentes lenguajes de programación.
+#Las listas contienen más de un elemento.
+#Se pueden ocupar elementos de diferentes tipos.
+
+lista = ["Lunes","Martes","Miercoles","Jueves","Viernes"] #Ejemplo de una lista de cadenas.
+lista2 = ["Lunes",1,5,7.8,"Viernes"]
+
+print("La lista 1 que se tiene es: ",lista)
+print("La segunda lista que se tiene es: ",lista2)
+
+#NOTA: Las listas tienen el mismo índice como los arreglos (empieza de 0).
+
+print("\n",lista[0:3]) #De la primera lista imprime los elementos que se encuentran en el índice 0 hasta el dos.
+print("\n",lista2[2:5]) # De la segunda lista imprime los elementos que se encuentran en el índice 2 hasta el 4.
+
+#Siempre será n-1.
