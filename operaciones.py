dc464bfd4d5ef6f50b52389b5d22c5aa9266fc98
boquer <dani_981103@hotmail.com>
2019-12-27 19:12:39 -0600

    3- Uso de operadores

---
+++ operaciones.py
@@ -0,0 +1,18 @@
+numero1 = 10
+numero2 = 5
+
+suma = numero1 + numero2 #suma
+resta = numero1 - numero2 #resta
+multi = numero1 * numero2 #multiplacion
+div = numero1 / numero2 #division
+exp = numero1 ** numero2  #se eleva el número 10 a la 5
+mod = numero1 % numero2 #nos muestra el residuo de la division
+
+'''Impresion de datos'''
+
+print("La suma es: ",suma)
+print("La resta es: ",resta)
+print("La multiplicacion es: ",multi)
+print("La division es: ",div)
+print("El resultado de 10 a la 5 es: ",exp)
+print("El residuo de la division es: ",mod)
